bfdad5677bfcacca10d5841c5ffe7f520eaa3edf
mikalinin <93651978+mikalinin@users.noreply.github.com>
2023-11-06 12:41:37 +0300

    Code review taskF.cpp

--- 1contest/taskF.cpp
+++ 1contest/taskF.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 #include <vector>
-// #include <algorithm>
 
 int QuickSelect(std::vector<int>& elems, int stat, int elem_num, int left,
                 int right) {
@@ -12,17 +11,17 @@ int QuickSelect(std::vector<int>& elems, int stat, int elem_num, int left,
   int second_uk = right;
   while (first_uk <= second_uk) {
     while (elems[first_uk] < mid) {
-      first_uk++;
+      ++first_uk;
     }
     while (elems[second_uk] > mid) {
-      second_uk--;
+      --second_uk;
     }
     if (first_uk <= second_uk) {
       int buffer_elem = elems[first_uk];
       elems[first_uk] = elems[second_uk];
       elems[second_uk] = buffer_elem;
-      first_uk++;
-      second_uk--;
+      ++first_uk;
+      --second_uk;
     }
   }
   if ((first_uk - left) >= stat) {
@@ -45,14 +44,12 @@ int main() {
   std::vector<int> elems(elem_num, 0);
   elems[0] = elem1;
   elems[1] = elem2;
-  for (int i = 2; i < elem_num; i++) {
+  for (int i = 2; i < elem_num; ++i) {
     elems[i] = ((elems[i - 2] * kA45) + (elems[i - 1] * kA123)) %
                (kA10000000 + kA4321);
   }
   int ans;
   ans = QuickSelect(elems, stat, elem_num, 0, elem_num - 1);
   std::cout << ans;
-  // std::sort(elems.begin(), elems.end());
-  // std::cout << elems[stat - 1];
   return 0;
 }
