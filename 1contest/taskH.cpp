eae80d0abe385ff7042e914c93714671e4eb0fab
mikalinin <93651978+mikalinin@users.noreply.github.com>
2023-11-06 12:57:44 +0300

    Code review taskH.cpp

--- 1contest/taskH.cpp
+++ 1contest/taskH.cpp
@@ -19,10 +19,10 @@ int Partition(std::vector<int>& elems, int left, int right) {
   int second_uk = right;
   while (first_uk <= second_uk) {
     while (elems[first_uk] < x) {
-      first_uk++;
+      ++first_uk;
     }
     while (elems[second_uk] > x) {
-      second_uk--;
+      --second_uk;
     }
     if (first_uk >= second_uk) {
       break;
@@ -31,8 +31,8 @@ int Partition(std::vector<int>& elems, int left, int right) {
     buffer_elem = elems[second_uk];
     elems[second_uk] = elems[first_uk];
     elems[first_uk] = buffer_elem;
-    first_uk++;
-    second_uk--;
+    ++first_uk;
+    --second_uk;
   }
   return second_uk;
 }
@@ -42,12 +42,12 @@ int main() {
   std::cin >> elem_num;
   std::vector<int> elems(elem_num);
   int new_elem;
-  for (int i = 0; i < elem_num; i++) {
+  for (int i = 0; i < elem_num; ++i) {
     std::cin >> new_elem;
     elems[i] = new_elem;
   }
   QuickSort(elems, 0, elem_num - 1);
-  for (int i = 0; i < elem_num; i++) {
+  for (int i = 0; i < elem_num; ++i) {
     std::cout << elems[i] << " ";
   }
   return 0;
