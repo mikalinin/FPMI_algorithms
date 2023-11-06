6c4647ee288994c096a01a6b741835c236024f5d
mikalinin <93651978+mikalinin@users.noreply.github.com>
2023-11-06 12:53:41 +0300

    Code review taskG.cpp

--- 1contest/taskG.cpp
+++ 1contest/taskG.cpp
@@ -1,27 +1,30 @@
 #include <iostream>
 #include <vector>
 
+void LSD(std::vector<unsigned long long>& elems, int elem_num) {
+  const int kMax = 63;
+  std::vector<std::vector<unsigned long long>> mass(
+      2, std::vector<unsigned long long>());
+  for (int i = 0; i <= kMax; ++i) {
+    for (int j = 0; j < elem_num; ++j) {
+      mass[(elems[j] >> i) % 2].push_back(elems[j]);
+    }
+    elems.clear();
+    for (int j = 0; j < 2; ++j) {
+      elems.insert(elems.end(), mass[j].begin(), mass[j].end());
+      mass[j].clear();
+    }
+  }
+}
+
 int main() {
-  const int kTwo = 2;
   int elem_num;
   std::cin >> elem_num;
   std::vector<unsigned long long> elems(elem_num);
   for (int i = 0; i < elem_num; ++i) {
     std::cin >> elems[i];
   }
-  const int kMax = 63;
-  std::vector<std::vector<unsigned long long>> mass(
-      kTwo, std::vector<unsigned long long>());
-  for (int i = 0; i <= kMax; ++i) {
-    for (int j = 0; j < elem_num; ++j) {
-      mass[(elems[j] >> i) % kTwo].push_back(elems[j]);
-    }
-    elems.clear();
-    for (int j = 0; j < kTwo; ++j) {
-      elems.insert(elems.end(), mass[j].begin(), mass[j].end());
-      mass[j].clear();
-    }
-  }
+  LSD(elems, elem_num);
   for (int i = 0; i < elem_num; ++i) {
     std::cout << elems[i] << '\n';
   }
