2d517cb8f28c23afc25d5d29ee9cd347c8b15cd2
mikalinin <93651978+mikalinin@users.noreply.github.com>
2023-12-01 18:28:59 +0300

    Update taskD.cpp

--- 2contest/taskD.cpp
+++ 2contest/taskD.cpp
@@ -3,121 +3,142 @@
 #include <stack>
 #include <string>
 
-struct Pair {
-  int elem;
-  int min;
+class Minqueue {
+  struct Pair {
+    int elem;
+    int min;
+  };
+  std::stack<Pair> stack1_;
+  int stack1_size_ = 0;
+  std::stack<Pair> stack2_;
+  int stack2_size_ = 0;
+
+ public:
+  void Enquery(int num) {
+    if (stack1_size_ == 0) {
+      stack1_.push(Pair(num, num));
+      ++stack1_size_;
+    } else {
+      int last_min = stack1_.top().min;
+      stack1_.push(Pair(num, std::min(num, last_min)));
+      ++stack1_size_;
+    }
+  }
+
+  int Dequeue() {
+    int ans = 0;
+    if (stack2_size_ == 0) {
+      while (stack1_size_ > 0) {
+        int elem = stack1_.top().elem;
+        stack1_.pop();
+        --stack1_size_;
+        int min = 0;
+        if (stack2_size_ == 0) {
+          min = elem;
+        } else {
+          min = std::min(elem, stack2_.top().min);
+        }
+        stack2_.push(Pair(elem, min));
+        ++stack2_size_;
+      }
+      ans = stack2_.top().elem;
+      stack2_.pop();
+      --stack2_size_;
+    } else {
+      ans = stack2_.top().elem;
+      stack2_.pop();
+      --stack2_size_;
+    }
+    return ans;
+  }
+
+  int Front() {
+    int ans = 0;
+    if (stack2_size_ == 0) {
+      while (stack1_size_ > 0) {
+        int elem = stack1_.top().elem;
+        stack1_.pop();
+        --stack1_size_;
+        int min = 0;
+        if (stack2_size_ == 0) {
+          min = elem;
+        } else {
+          min = std::min(elem, stack2_.top().min);
+        }
+        stack2_.push(Pair(elem, min));
+        ++stack2_size_;
+      }
+      ans = stack2_.top().elem;
+    } else {
+      ans = stack2_.top().elem;
+    }
+    return ans;
+  }
+
+  int Min() {
+    int ans = 0;
+    if (stack2_size_ == 0) {
+      ans = stack1_.top().min;
+    } else if (stack1_size_ == 0) {
+      ans = stack2_.top().min;
+    } else {
+      ans = std::min(stack1_.top().min, stack2_.top().min);
+    }
+    return ans;
+  }
+
+  int Size() const { return stack1_size_ + stack2_size_; }
+
+  void Clear() {
+    std::stack<Pair>().swap(stack1_);
+    std::stack<Pair>().swap(stack2_);
+    stack1_size_ = 0;
+    stack2_size_ = 0;
+  }
+
+  bool IsVoid() const { return stack1_size_ == 0 && stack2_size_ == 0; }
 };
-
-void Enquery(std::stack<Pair>& stack1, int& stack1_size, int num) {
-  if (stack1_size == 0) {
-    stack1.push(Pair(num, num));
-    stack1_size++;
-  } else {
-    int last_min = stack1.top().min;
-    stack1.push(Pair(num, std::min(num, last_min)));
-    stack1_size++;
-  }
-}
-
-void Dequeue(std::stack<Pair>& stack1, int& stack1_size,
-             std::stack<Pair>& stack2, int& stack2_size) {
-  if (stack1_size == 0 && stack2_size == 0) {
-    std::cout << "error"
-              << "\n";
-  } else if (stack2_size == 0) {
-    while (stack1_size > 0) {
-      int elem = stack1.top().elem;
-      stack1.pop();
-      stack1_size--;
-      int min = 0;
-      if (stack2_size == 0) {
-        min = elem;
-      } else {
-        min = std::min(elem, stack2.top().min);
-      }
-      stack2.push(Pair(elem, min));
-      stack2_size++;
-    }
-    std::cout << stack2.top().elem << '\n';
-    stack2.pop();
-    stack2_size--;
-  } else {
-    std::cout << stack2.top().elem << '\n';
-    stack2.pop();
-    stack2_size--;
-  }
-}
-
-void Front(std::stack<Pair>& stack1, int& stack1_size, std::stack<Pair>& stack2,
-           int& stack2_size) {
-  if (stack1_size == 0 && stack2_size == 0) {
-    std::cout << "error"
-              << "\n";
-  } else if (stack2_size == 0) {
-    while (stack1_size > 0) {
-      int elem = stack1.top().elem;
-      stack1.pop();
-      stack1_size--;
-      int min = 0;
-      if (stack2_size == 0) {
-        min = elem;
-      } else {
-        min = std::min(elem, stack2.top().min);
-      }
-      stack2.push(Pair(elem, min));
-      stack2_size++;
-    }
-    std::cout << stack2.top().elem << '\n';
-  } else {
-    std::cout << stack2.top().elem << '\n';
-  }
-}
-
-void Min(std::stack<Pair>& stack1, int& stack1_size, std::stack<Pair>& stack2,
-         int& stack2_size) {
-  if (stack1_size == 0 && stack2_size == 0) {
-    std::cout << "error"
-              << "\n";
-  } else if (stack2_size == 0) {
-    std::cout << stack1.top().min << "\n";
-  } else if (stack1_size == 0) {
-    std::cout << stack2.top().min << "\n";
-  } else {
-    std::cout << std::min(stack1.top().min, stack2.top().min) << "\n";
-  }
-}
 
 int main() {
   std::string query;
-  std::stack<Pair> stack1;
-  std::stack<Pair> stack2;
-  int stack1_size = 0;
-  int stack2_size = 0;
   int query_num;
+  Minqueue queue;
   std::cin >> query_num;
   for (int i = 0; i < query_num; i++) {
     std::cin >> query;
     if (query == "enqueue") {
       int num;
       std::cin >> num;
-      Enquery(stack1, stack1_size, num);
+      queue.Enquery(num);
       std::cout << "ok"
                 << "\n";
     } else if (query == "dequeue") {
-      Dequeue(stack1, stack1_size, stack2, stack2_size);
+      if (queue.IsVoid()) {
+        std::cout << "error"
+                  << "\n";
+      } else {
+        std::cout << queue.Dequeue() << "\n";
+      }
     } else if (query == "front") {
-      Front(stack1, stack1_size, stack2, stack2_size);
+      if (queue.IsVoid()) {
+        std::cout << "error"
+                  << "\n";
+      } else {
+        std::cout << queue.Front() << "\n";
+      }
     } else if (query == "size") {
-      std::cout << stack1_size + stack2_size << "\n";
+      std::cout << queue.Size() << "\n";
     } else if (query == "clear") {
-      std::stack<int> stack1;
-      std::stack<int> stack2;
-      stack1_size = 0;
-      stack2_size = 0;
+      queue.Clear();
       std::cout << "ok"
                 << "\n";
     } else if (query == "min") {
-      Min(stack1, stack1_size, stack2, stack2_size);
+      if (queue.IsVoid()) {
+        std::cout << "error"
+                  << "\n";
+      } else {
+        std::cout << queue.Min() << "\n";
+      }
     }
   }
   return 0;
